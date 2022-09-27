fe7a850dc04566b61013bd09cee413c1b3bac2b7
mitchbecker <104916860+mitchbecker@users.noreply.github.com>
2022-09-27 10:42:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ apt install nginx jq curl -y
 
 # configure nginx
 rm -r /var/www/html/*
-wget -O /var/www/html/index.html https://raw.githubusercontent.com/medsteele/iss-server-scripts/main/index.html
+wget -O /var/www/html/index.html https://raw.githubusercontent.com/mitchbecker/iss-server-scripts/main/index.html
 
 # create scripts directory & download scripts
 rm -r /scripts
 mkdir /scripts
-wget -O /scripts/install.sh https://raw.githubusercontent.com/medsteele/iss-server-scripts/main/install.sh
-wget -O /scripts/update-webpage.sh https://raw.githubusercontent.com/medsteele/iss-server-scripts/main/update-webpage.sh
+wget -O /scripts/install.sh https://raw.githubusercontent.com/mitchbecker/iss-server-scripts/main/install.sh
+wget -O /scripts/update-webpage.sh https://raw.githubusercontent.com/mitchbecker/iss-server-scripts/main/update-webpage.sh
 
 # create crontab
 echo "* * * * * sh /scripts/update-webpage.sh" >> /var/tmp/cronconfig
