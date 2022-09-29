526425fa7ea11785aa8bc466a315e01f27fa0ec6
mitchbecker <104916860+mitchbecker@users.noreply.github.com>
2022-09-29 10:08:20 -0400

    Update update-iss-stats.sh

--- update-iss-stats.sh
+++ update-iss-stats.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 echo "current datetime: $(date) : " >> /var/www/html/index.html
 curl http://api.open-notify.org/iss-now.json | jq .iss_position >> /var/www/html/index.html
 echo "<br/>" >> /var/www/html/index.html
