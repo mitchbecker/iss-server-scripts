b5965f42d129087bb61a97367c289742ec0fefec
mitchbecker <104916860+mitchbecker@users.noreply.github.com>
2022-09-29 09:49:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # upgrade system
 DEBIAN_FRONTEND=noninteractive
 apt update && apt upgrade -y
@@ -7,17 +5,17 @@ apt update && apt upgrade -y
 # install nginx & jq & curl
 apt install nginx jq curl -y
 
-# configure nginx
-rm -r /var/www/html/*
-wget -O /var/www/html/index.html https://raw.githubusercontent.com/mitchbecker/iss-server-scripts/main/index.html
-
-# create scripts directory & download scripts
+# create scripts directory
 rm -r /scripts
 mkdir /scripts
-wget -O /script/install.sh https://raw.githubusercontent.com/mitchbecker/iss-server-scripts/main/install.sh
-wget -O /script/update-iss-stats.sh https://raw.githubusercontent.com/mitchbecker/iss-server-scripts/main/update-iss-stats.sh
+
+# configure nginx
+rm /var/www/html/*
+
+# create script file & index page
+wget -O /scripts/update-iss-stats.sh https://raw.githubusercontent.com/gsantella/iss-server-scripts/main/update-iss-stats.sh
 
 # create crontab
-echo "* * * * * sh /script/update-iss-stats.sh" >> /var/tmp/cronconfig
+echo "* * * * * sh /scripts/update-iss-stats.sh" >> /var/tmp/cronconfig
 crontab /var/tmp/cronconfig
 rm /var/tmp/cronconfig
